79a8c540ff16d6801b02d7b82c455b3564225c82
Garrett <gbland777@gmail.com>
2016-09-21 00:46:24 -0500

    Firebase api call working

--- app/scenes/Favorites.js
+++ app/scenes/Favorites.js
@@ -5,6 +5,7 @@ import { bindActionCreators } from 'redux';
 import * as ReduxActions from '../actions/';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import { Actions } from 'react-native-router-flux';
+import * as firebase from 'firebase';
 
 import {
   AppRegistry,
@@ -13,14 +14,56 @@ import {
   View,
   TouchableOpacity,
   Alert,
+  ListView,
+  Image,
 } from 'react-native';
 
+const firebaseConfig = {
+  databaseURL: "https://eathays-f5100.firebaseio.com/",
+};
+const firebaseApp = firebase.initializeApp(firebaseConfig);
 
 class Favorites extends Component {
 
   constructor(props) {
     super(props);
-    this.state = { /* initial state */ };
+    this.state = {
+      dataSource: new ListView.DataSource({
+        rowHasChanged: (row1, row2) => row1 !== row2,
+      })
+    };
+    this.itemsRef = this.getRef().child('placesToEat');
+    console.log(this.itemsRef);
+  }
+
+  getRef() {
+    return firebaseApp.database().ref();
+  }
+
+  listenForItems(itemsRef) {
+    itemsRef.on('value', (snap) => {
+
+      // get children as an array
+      var items = [];
+      snap.forEach((child) => {
+        items.push({
+          title: child.val().title,
+          profile:child.val().profile,
+          _key: child.key,
+        });
+      });
+
+      console.log(items);
+
+      this.setState({
+        dataSource: this.state.dataSource.cloneWithRows(items)
+      });
+
+    });
+  }
+
+  componentDidMount() {
+    this.listenForItems(this.itemsRef);
   }
 
   componentWillMount(){
@@ -34,12 +77,29 @@ class Favorites extends Component {
         <Text style={styles.welcome}>
           Favorites
         </Text>
-        <Text style={styles.instructions}>
-          The props are {this.props.text}
-        </Text>
+        <ListView
+            dataSource={this.state.dataSource}
+            renderRow={this._renderItem.bind(this)}
+            enableEmptySections={true}
+        />
       </View>
     );
   }
+
+  _renderItem(item) {
+
+  return (
+    <View>
+    <Text>{item.title}</Text>
+    <Image
+      onLoadStart={() => this.setState({loading:true})}
+      onLoadEnd={() => this.setState({loading:false})}
+      style={{width: 100, height: 100}}
+      source={{uri: item.profile}}
+    />
+    </View>
+  );
+}
 }
 
 const styles = StyleSheet.create({
