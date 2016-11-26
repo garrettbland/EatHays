b5d11d589f8113919ce89e0c65b6595bf4618663
Garrett Bland <garrettbland@Garretts-MacBook-Air.local>
2016-11-26 00:15:28 -0600

    Review full list code cleanup

--- app/scenes/ReviewFullList.js
+++ app/scenes/ReviewFullList.js
@@ -1,115 +1,81 @@
 import React, { Component } from 'react';
-
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import * as ReduxActions from '../actions/';
-import Icon from 'react-native-vector-icons/FontAwesome';
-import { Actions } from 'react-native-router-flux';
-import {Button, Card} from 'react-native-elements';
+import { Card } from 'react-native-elements';
 import firebaseApp from "../components/firebaseconfig.js";
 import {
-  AppRegistry,
-  StyleSheet,
   Text,
   View,
-  TouchableOpacity,
-  Alert,
   ListView,
-  Dimensions,
   Platform,
   ScrollView,
 } from 'react-native';
 
-const screenWidth = Dimensions.get('window').width;
-const screenHeight = Dimensions.get('window').height;
-
 class ReviewFullList extends Component {
 
   constructor(props) {
-     super(props);
-     const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+    super(props);
+    const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+    var dataSource = this.props.reviews;
+    var dataSourceReversed = dataSource.reverse();
+    this.state = {
+     reviewsDataSource: ds.cloneWithRows(dataSourceReversed),
+    };
+    this.itemsRef = this.getRef().child("directoryStatistics");
+  }
 
-     var dataSource = this.props.reviews;
-     var dataSourceReversed = dataSource.reverse();
-       this.state = {
-         reviewsDataSource: ds.cloneWithRows(dataSourceReversed),
-       };
-
-
-      this.itemsRef = this.getRef().child("directoryStatistics");
-
-
-   }
-
-   getRef() {
-     return firebaseApp.database().ref();
-   }
-
-   _renderItem(item) {
-     if (item.review == "null"){
-       return (
-         <View>
-         <Card
-            >
-            <Text style={{color:'#c0392b',fontFamily:'oswald-regular',fontSize:15,marginBottom:6}}>
-            No Reviews yet, be the first!
-            </Text>
-            <Text style={{fontStyle:'italic',color:'#7f8c8d',fontSize:13}}>No sign up required</Text>
-          </Card>
-         </View>
-       )
-     }else{
-           return (
-             <View >
-               <Card
-                  >
-                  <Text style={{color:'#c0392b',fontFamily:'oswald-regular',fontSize:15,marginBottom:6}}>
-                  {item.review}
-                  </Text>
-
-                  <Text style={{fontStyle:'italic',color:'#7f8c8d',fontSize:13}}>{item.name} on {item.date}</Text>
-                </Card>
-             </View>
-           );
-         }
-       }
-
-
+  getRef() {
+    return firebaseApp.database().ref();
+  }
 
   render() {
     return (
-      <View style={styles.container}>
-      <ScrollView>
-        <Text style={styles.welcomeDay}>
-          {this.props.restaurantTitle} Reviews
-        </Text>
-        <ListView
-            dataSource={this.state.reviewsDataSource}
-            renderRow={this._renderItem.bind(this)}
-            enableEmptySections={true}
-        />
+      <View style={{paddingTop:Platform.OS === 'ios'? 64 : 54,backgroundColor:'#ffffff',flex:1}}>
+        <ScrollView>
+          <Text style={{fontSize: 40,textAlign: 'center',margin: 10,paddingTop:20,paddingBottom:15,fontFamily:'oswald-bold',color:"black"}}>
+            {this.props.restaurantTitle} Reviews
+          </Text>
+          <ListView
+              dataSource={this.state.reviewsDataSource}
+              renderRow={this._renderItem.bind(this)}
+              enableEmptySections={true}
+          />
         </ScrollView>
       </View>
     );
   }
-}
 
-const styles = StyleSheet.create({
-  container: {
-    paddingTop:Platform.OS === 'ios'? 64 : 54,
-    backgroundColor:'#ffffff',
-    flex:1,
-  },
-  welcomeDay: {
-    fontSize: 40,
-    textAlign: 'center',
-    margin: 10,
-    paddingTop:20,
-    paddingBottom:15,
-    fontFamily:'oswald-bold',
-    color:"black",
-  }
-});
+  _renderItem(item) {
+     if (item.review == "null"){
+       return (
+         <View>
+           <Card>
+             <Text style={{color:'#c0392b',fontFamily:'oswald-regular',fontSize:15,marginBottom:6}}>
+               No Reviews yet, be the first!
+             </Text>
+             <Text style={{fontStyle:'italic',color:'#7f8c8d',fontSize:13}}>
+               No sign up required
+             </Text>
+           </Card>
+         </View>
+       );
+    }else{
+       return (
+         <View >
+           <Card>
+             <Text style={{color:'#c0392b',fontFamily:'oswald-regular',fontSize:15,marginBottom:6}}>
+               {item.review}
+             </Text>
+             <Text style={{fontStyle:'italic',color:'#7f8c8d',fontSize:13}}>
+               {item.name} on {item.date}
+             </Text>
+           </Card>
+         </View>
+       );
+     }
+   }
+}
 
 function mapStateToProps(state){
   return {
