49ee824270c147151bced2106ff2591179a4b91a
Garrett Bland <garrettbland@Garretts-MacBook-Air.local>
2016-11-25 23:26:59 -0600

    Special Detail code cleanup - foreground image

--- app/scenes/SpecialDetail.js
+++ app/scenes/SpecialDetail.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import * as ReduxActions from '../actions/';
@@ -9,12 +8,11 @@ import ParallaxScrollView from 'react-native-parallax-scroll-view';
 import {Button} from 'react-native-elements';
 import firebaseApp from "../components/firebaseconfig.js";
 import moment from 'moment';
-
+import Image from 'react-native-image-progress';
 import {
   StyleSheet,
   Text,
   View,
-  Image,
   Alert,
   ActivityIndicator,
   Dimensions,
@@ -26,18 +24,14 @@ import {
 const screenWidth = Dimensions.get('window').width;
 const screenHeight = Dimensions.get('window').height;
 
-
 class SpecialDetail extends Component {
 
   constructor(props) {
     super(props);
     this.state = {
-      item:{},
       loading:true,
     };
-
     this.recordItemsRef = this.getRef().child("specialsStatistics");
-
   }
 
   getRef() {
@@ -53,19 +47,16 @@ class SpecialDetail extends Component {
     var unixTimeStamp = moment().unix();
     var userPlatform = Platform.OS === 'ios'? "ios" : "android";
     this.recordItemsRef.push({
-        restaurant:this.props.title,
-        platform:userPlatform,
-        visited: dateSubmitted,
-        timestamp:unixTimeStamp,
-        special:this.props.special,
-      });
+      restaurant:this.props.title,
+      platform:userPlatform,
+      visited: dateSubmitted,
+      timestamp:unixTimeStamp,
+      special:this.props.special,
+    });
   }
 
-
   listenForItems(itemsRef) {
-
     itemsRef.orderByChild("title").startAt(this.props.title).endAt(this.props.title).on('value', (snap) => {
-
       items = [];
       snap.forEach((child) => {
         items.push({
@@ -89,11 +80,9 @@ class SpecialDetail extends Component {
       });
 
       var stringify = JSON.stringify(items);
-
       this.setState({
         items:stringify.toString()
       });
-
     });
   }
 
@@ -103,110 +92,60 @@ class SpecialDetail extends Component {
   }
 
   render() {
-
     if(this.state.items == undefined){
-
+      return(<View></View>);
     }else{
       var itemz = JSON.parse(this.state.items);
       var item = itemz[0];
-
     }
     return (
-      <View style={styles.container}>
-      <ParallaxScrollView
-           fadeOutForeground={false}
-           renderBackground={() => <Image source={{ uri: this.props.background, width: window.width, height: screenHeight / 2 }} onLoadStart={() => this.setState({loading:true})} onLoad={() => this.setState({loading:false})}/>}
-           contentBackgroundColor="#ffffff"
-           parallaxHeaderHeight={screenHeight / 2}
-           renderForeground={() => (
-            <View style={{ height: 300, flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-               <Image resizeMode={'contain'} source={{ uri: this.props.profile, width: screenWidth /2, height: 100 }}/>
+      <View style={{paddingTop:Platform.OS === 'ios'? 64 : 54, backgroundColor:'#ffffff',flex:1}}>
+        <ParallaxScrollView
+             fadeOutForeground={false}
+             renderBackground={() => <Image source={{ uri: this.props.background, width: window.width, height: screenHeight / 2 }} onLoadStart={() => this.setState({loading:true})} onLoad={() => this.setState({loading:false})}/>}
+             contentBackgroundColor="#ffffff"
+             parallaxHeaderHeight={screenHeight / 2}>
+             <View style={{height:10,backgroundColor:'#c0392b',}}></View>
+             <View>
+               <View style={{flex: 1, paddingTop:12,alignItems: 'center', justifyContent: 'center' }}>
+                  <Image resizeMode={'contain'} source={{ uri: this.props.profile, width: screenWidth /2, height: 100 }} onLoadStart={() => this.setState({loading:true})} onLoad={() => this.setState({loading:false})} indicator={ActivityIndicator}/>
+               </View>
+               <Text style={{fontSize: 40,textAlign: 'center',margin: 10,paddingTop:20,paddingBottom:15,fontFamily:'oswald-bold',color:"black"}}>
+                  {this.props.title} {this.props.day} Special
+               </Text>
+               <Text style={{fontSize:screenWidth / 15,fontFamily:'oswald-regular',color:'#c0392b',textAlign:'center',padding:5,paddingBottom:15}}>
+                  {this.props.specialDescription}
+               </Text>
+               <Button
+                  raised
+                  iconRight
+                  borderRadius={5}
+                  icon={{name: 'chevron-right'}}
+                  fontFamily="oswald-bold"
+                  fontSize={18}
+                  buttonStyle={{marginBottom:10}}
+                  backgroundColor="#2bc064"
+                  title='View Restaurant'
+                  onPress={() => Actions.DirectoryDetail({title:this.props.title,item,specialHashTag:this.props.special})}
+                />
+                <Button
+                  raised
+                  iconRight
+                  borderRadius={5}
+                  icon={{name: 'chevron-right'}}
+                  fontFamily="oswald-bold"
+                  fontSize={18}
+                  buttonStyle={{marginBottom:5}}
+                  backgroundColor="#3498db"
+                  title='Review this meal'
+                  onPress={() => Actions.ReviewDetail({restaurantTitle:this.props.title,specialHashTag:this.props.special,fromSpecial:true})}
+                />
              </View>
-           )}>
-           <View style={{height:10,backgroundColor:'#c0392b',}}></View>
-           <View>
-               {
-                 this.state.loading &&
-
-                 <ActivityIndicator
-                   size="large"
-                   color="#3498db"
-                   style={styles.activityStyle}
-                 />
-
-               }
-              <Text style={styles.welcomeDay}>{this.props.title} {this.props.day} Special</Text>
-              <Text style={styles.description}>{this.props.specialDescription}</Text>
-              <Button
-                raised
-                iconRight
-                borderRadius={5}
-                icon={{name: 'chevron-right'}}
-                fontFamily="oswald-bold"
-                fontSize={18}
-                buttonStyle={{marginBottom:10,}}
-                backgroundColor="#2bc064"
-                title='View Restaurant'
-                onPress={() => Actions.DirectoryDetail({title:this.props.title,item,specialHashTag:this.props.special})}
-              />
-
-              <Button
-                raised
-                iconRight
-                borderRadius={5}
-                icon={{name: 'chevron-right'}}
-                fontFamily="oswald-bold"
-                fontSize={18}
-                buttonStyle={{marginBottom:5,}}
-                backgroundColor="#3498db"
-                title='Review this meal'
-                onPress={() => Actions.ReviewDetail({restaurantTitle:this.props.title,specialHashTag:this.props.special,fromSpecial:true})}
-              />
-           </View>
-         </ParallaxScrollView>
+        </ParallaxScrollView>
       </View>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    paddingTop:Platform.OS === 'ios'? 64 : 54,
-    backgroundColor:'#ffffff',
-    flex:1,
-  },
-  center: {
-    alignItems: 'center',
-  },
-  activityStyle:{
-    paddingTop:12,
-  },
-  welcomeDay: {
-    fontSize: 40,
-    textAlign: 'center',
-    margin: 10,
-    paddingTop:20,
-    paddingBottom:15,
-    fontFamily:'oswald-bold',
-    color:"black",
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-  description:{
-    fontSize:screenWidth / 15,
-    fontFamily:'oswald-regular',
-    color:'#c0392b',
-    textAlign:'center',
-    padding:5,
-    paddingBottom:15,
-  },
-  activityStyle:{
-    marginTop:5,
-  },
-});
 
 function mapStateToProps(state){
   return {
