8552878cf3268cffbbb5462f8b71fcc7cf3fb375
Garrett Bland <garrettbland@Garretts-MacBook-Air.local>
2016-11-25 23:31:15 -0600

    mapDetail code cleanup

--- app/scenes/MapDetail.js
+++ app/scenes/MapDetail.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import * as ReduxActions from '../actions/';
@@ -7,12 +6,8 @@ import Icon from 'react-native-vector-icons/FontAwesome';
 import { Actions } from 'react-native-router-flux';
 import {Button} from 'react-native-elements';
 import {
-  AppRegistry,
-  StyleSheet,
   Text,
   View,
-  TouchableOpacity,
-  Alert,
   WebView,
   Platform,
   ActivityIndicator,
@@ -25,41 +20,24 @@ class MapDetail extends Component {
     this.state = { loading:true };
   }
 
-
   render() {
     return (
-      <View style={styles.container}>
-      { this.state.loading &&
-        <ActivityIndicator
-          size="large"
-          color="#3498db"
-          style={{paddingTop:4}}
+      <View style={{flex: 1,paddingTop:Platform.OS === 'ios'? 64 : 54}}>
+        {this.state.loading &&
+          <ActivityIndicator
+            size="large"
+            color="#3498db"
+            style={{paddingTop:4}}
+          />
+        }
+        <WebView
+          onLoad={()=>this.setState({loading:false})}
+          source={{uri: this.props.addressURL}}
         />
-      }
-      <WebView
-        onLoad={()=>this.setState({loading:false})}
-        source={{uri: this.props.addressURL}}/>
       </View>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    paddingTop:Platform.OS === 'ios'? 64 : 54,
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
 
 function mapStateToProps(state){
   return {
